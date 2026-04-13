84815a3e9c0cbd9272ed1acccf827c632e5ea9ce
joshuaquitoy22@gmail.com <joshuaquits@gmail.com>
2026-04-13 11:09:36 +0800

    added input and properties

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,10 +1,62 @@
 import { Route, Routes } from 'react-router-dom';
 import AppLayout from '../layout/AppLayout';
+import FloatingLabelInput from '../components/input/FloatingLabelInput';
+import { useState } from 'react';
 
 const SampleComponent = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <>
-      <h1 className="text-blue-800">Hello Eberywone!</h1>
+      <h1 className="text-blue-800 mb-4">Hello Eberywone!</h1>
+      <div className="mb-4">
+        <FloatingLabelInput
+          label="First Name"
+          type="text"
+          name="first_name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+          autoFocus
+        />
+        <p className="font-medium">First Name: {firstName}</p>
+      </div>
+      <div className="mb-4">
+        <FloatingLabelInput
+          label="Last Name"
+          type="text"
+          name="last_name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+        <p className="font-medium">Last Name: {lastName}</p>
+      </div>
+      <div className="mb-4">
+        <FloatingLabelInput
+          label="Birth Date"
+          type="date"
+          name="birth_date"
+          value={birthDate}
+          onChange={(e) => setBirthDate(e.target.value)}
+          required
+        />
+        <p className="font-medium">Birth Date: {birthDate}</p>
+      </div>
+      <div className="mb-4">
+        <FloatingLabelInput
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+          required
+        />
+        <p className="font-medium">Password: {password} </p>
+      </div>
     </>
   );
 };
